abda75d95cce7e25413b0ff71a422e0144ab4d35
dror <drorpilot@gmail.com>
2019-08-20 15:45:14 +0300

    prepare for recycler view adapter

--- app/src/main/java/com/example/org/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/org/coderswag/Controller/MainActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Adapter
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.org.coderswag.Adapters.CategoryAdapter
 import com.example.org.coderswag.Model.Category
 import com.example.org.coderswag.R
 import com.example.org.coderswag.Services.DataService
@@ -11,16 +13,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    //lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapter = ArrayAdapter(
-                this,
-        android.R.layout.simple_list_item_1,
-        DataService.categories
-        )
+//        adapter = ArrayAdapter(
+//                this,
+//        android.R.layout.simple_list_item_1,
+//        DataService.categories
+//        )
+        adapter = CategoryAdapter(this, DataService.categories)
+
         categoryListView.adapter = adapter
+
+        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+            val category = DataService.categories[i]
+            Toast.makeText(this, "You clicked the ${category.title} cell", Toast.LENGTH_SHORT).show()
+        }
     }
 }
