a3bd36b247bbc0fe42db17f4ca3f916bef8ab68c
dror <drorpilot@gmail.com>
2019-08-21 08:07:47 +0300

    have bug here somewhere

--- app/src/main/java/com/example/org/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/org/coderswag/Controller/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.org.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.Adapter
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
 import android.widget.Toast
 import com.example.org.coderswag.Adapters.CategoryAdapter
+import com.example.org.coderswag.Adapters.CategoryRecycleAdapter
 import com.example.org.coderswag.Model.Category
 import com.example.org.coderswag.R
 import com.example.org.coderswag.Services.DataService
@@ -14,7 +17,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     //lateinit var adapter: ArrayAdapter<Category>
-    lateinit var adapter: CategoryAdapter
+    //lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +28,18 @@ class MainActivity : AppCompatActivity() {
 //        android.R.layout.simple_list_item_1,
 //        DataService.categories
 //        )
-        adapter = CategoryAdapter(this, DataService.categories)
+        //adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
 
-        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
-            val category = DataService.categories[i]
-            Toast.makeText(this, "You clicked the ${category.title} cell", Toast.LENGTH_SHORT).show()
-        }
+//        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+//            val category = DataService.categories[i]
+//            Toast.makeText(this, "You clicked the ${category.title} cell", Toast.LENGTH_SHORT).show()
+//        }
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
